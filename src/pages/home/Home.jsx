5192ab792526503ad3517a1930b2b4ba8de4d523
BombMarley <kile146@gmail.com>
2023-03-26 21:07:56 +0200

    Add RecipeList , fetching single recipe

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,20 @@
+import { useFetch } from '../../hooks/useFetch'
+
+//styles
 import './Home.css'
 
+//components
+import RecipesList from '../../components/RecipesList'
+
+
 export default function Home() {
+  const { data, isPending, error} = useFetch('http://localhost:3000/recipes')
+
   return (
-    <div>
-      Home
+    <div className='home'>
+      {error && <p className='error'>{error}</p>}
+      {isPending && <p className='loading'>Loading...</p>}
+      {data && <RecipesList recipes={data} />}
     </div>
   )
 }
