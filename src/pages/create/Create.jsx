b02421f639b8e9769a63dcfe703f3a39f0bfbeb3
BombMarley <kile146@gmail.com>
2023-03-27 17:40:07 +0200

    Add POST request and redirect the user

--- src/pages/create/Create.jsx
+++ src/pages/create/Create.jsx
@@ -1,4 +1,6 @@
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import { useFetch } from '../../hooks/useFetch'
 
 //styles
 import './Create.css'
@@ -11,10 +13,13 @@ export default function Create() {
   const [ingredients, setIngredients] = useState([])
 
   const ingredintInput = useRef(null)
+  const history = useHistory()
+
+  const { postData, data, error } = useFetch('http://localhost:3000/recipes/', 'POST')
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log(title, method, cookingTime, ingredients);
+    postData({title, ingredients, method, cookingTime: cookingTime + ' minutes'})
   }
 
   const handleAdd = (e) => {
@@ -27,6 +32,13 @@ export default function Create() {
     setNewIngredient('')
     ingredintInput.current.focus()
   }
+
+  //redirect the user when we get data response
+  useEffect(() => {
+    if (data) {
+      history.push('/')
+    }
+  }, [data])
 
   return (
     <div className='create'>
